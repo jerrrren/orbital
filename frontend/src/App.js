52ae53cdbc744769b156f527a62b5a620fff26a7
bojie3 <95684564+bojie3@users.noreply.github.com>
2022-06-20 10:21:04 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './components/login/login';
 import Logout from './components/login/logout'
-import LandingPage from './components/ladingpage/landingpage'
-import Register from "./components/registration/register"
+import LandingPage from './components/landingpage/landingpage'
+import Register from "./components/register/register"
 import LoggedInHome from './components/loggedin/home/home';
 import FriendPage from './components/loggedin/friendPage/friendPage';
+import Contents from './components/loggedin/home/postContent';
+import Addpost from './components/loggedin/post/addPost';
 
 function App() {
 
@@ -19,6 +21,8 @@ function App() {
           <Route path="/login" exact element={<Login />} />
           <Route path="/register" exact element={<Register />} />
           <Route path="/logout" exact element={<Logout />} />
+          <Route path="/post/content/:id" element={<Contents />} />
+          <Route path="/post/add" element={< Addpost />} />
         </Routes>
       </BrowserRouter>
     </div>
