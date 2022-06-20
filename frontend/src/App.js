da2302e159bf52595eb8dc4328fd2506c46411ca
jerrrren <cheejeren@yahoo.com.sg>
2022-06-20 15:41:25 +0800

    update changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './components/login/login';
+import { ChakraProvider } from "@chakra-ui/react";
+
+import Login from './components/login/login'
 import Logout from './components/login/logout'
-import LandingPage from './components/landingpage/landingpage'
-import Register from "./components/register/register"
+import LandingPage from "./components/landingpage/landingpage";
+import Register from './components/registration/register'
+
 import LoggedInHome from './components/loggedin/home/home';
 import FriendPage from './components/loggedin/friendPage/friendPage';
 import ChatPage from './components/loggedin/chatPage/chatPage'
 
-import { ChakraProvider } from '@chakra-ui/react'
 import Contents from './components/loggedin/home/postContent';
 import Addpost from './components/loggedin/post/addPost';
 
@@ -19,18 +21,19 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" exact element={<LandingPage />} />
+          <Route path="/register" exact element={<Register />} />
+
+
           <Route path="/home/:id" element={<LoggedInHome />} />
           <Route path="/friends/:id" element={<FriendPage />} />
           <Route path="/login" exact element={<Login />} />
-          <Route path="/register" exact element={<Register />} />
           <Route path="/logout" exact element={<Logout />} />
           <Route path="/post/content/:id" element={<Contents />} />
-          <Route path = "chat/:id"></Route>
-          <Route path="/post/add" element={< Addpost />} />
+          <Route path="/chat/:id" element={<ChatPage />} />
+          <Route path="/post/add" element={<Addpost />} />
         </Routes>
       </BrowserRouter>
     </ChakraProvider>
-    
   );
 }
 
