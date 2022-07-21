becd005f9be61c031d2863d74f1b1e76ddc17785
bojie3 <95684564+bojie3@users.noreply.github.com>
2022-07-21 10:20:16 +0800

    update to session storage

--- myfrontend/src/components/loggedin/friendPage/friendPage.js
+++ myfrontend/src/components/loggedin/friendPage/friendPage.js
@@ -3,36 +3,46 @@ import { Navigate } from "react-router-dom";
 import LoggedinNavbar from "../navigation/nav";
 import PairingPage from "../findingPartner/findingpartner";
 import axios from "axios";
-import WaitingPage from "../waiting/waitingPage";
 import ChatPage from "../chatPage/chatPage";
+import { url } from "../../../constants/url";
+
 
 //page to be loaded when friend button on navbar is chosen
 //will either load the findPartner page, waiting page or the chat page
 const FriendPage = () => {
-    const [paired, setPaired] = useState(false)
-    const username = localStorage.getItem("username")
+  const [paired, setPaired] = useState(false);
+  const username = sessionStorage.getItem("username");
 
-    useEffect(() => {
-        axios
-          .post("http://localhost:8080/pairing/ifPaired", {
-            Name: username,
-          })
-          .then((resp) => {
-            setPaired(resp.data.message);
-          })
-          .catch((err) => console.log(err));
-    }, [])
+  useEffect(() => {
+    axios
+      .post(url.if_paired, {
+        Name: username,
+      })
+      .then((resp) => {
+        setPaired(resp.data.message);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    return (
+  return (
+    <div>
+      {paired ? (
+        <ChatPage />
+      ) : (
         <div>
-            {paired ? <ChatPage /> : <div><div className="jumbotron p-3 p-md-5 text-white rounded bg-dark">
-                <div className="col-md-6 px-0">
-                    <h1 className="display-4 font-italic">You have yet to be paired, please go to the pair tab on the navigation bar</h1>
-                </div>
-            </div></div>}
+          <LoggedinNavbar />
+          <div className="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+            <div className="col-md-6 px-0">
+              <h1 className="display-4 font-italic">
+                You have yet to be paired, please go to the pair tab on the
+                navigation bar
+              </h1>
+            </div>
+          </div>
         </div>
+      )}
+    </div>
+  );
+};
 
-    )
-}
-
-export default FriendPage
+export default FriendPage;
